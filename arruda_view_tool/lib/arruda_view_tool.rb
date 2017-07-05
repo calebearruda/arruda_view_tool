b5026e4f77beef0f4979f86dd8a148911ba06529
Calebe Arruda <calebearruda@gmail.com>
2017-07-04 22:18:46 -0300

    Initialize the repo

---
+++ arruda_view_tool/lib/arruda_view_tool.rb
@@ -0,0 +1,6 @@
+require "arruda_view_tool/version"
+require "arruda_view_tool/renderer"
+
+module ArrudaViewTool
+  # Your code goes here...
+end
---
+++ arruda_view_tool/lib/arruda_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ArrudaViewTool
+  VERSION = "0.1.0"
+end
